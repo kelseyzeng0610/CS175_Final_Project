35d0593908ae1ccca3f74ca4b88c2dfae3293b86
xiaocheng sun <xiaocheng.sun@tufts.edu>
2024-12-09 13:00:27 -0500

    added shape options

--- main.cpp
+++ main.cpp
@@ -24,6 +24,8 @@
 #include <FL/glut.h>
 #include <FL/glu.h>
 
+#include <FL/Fl_Round_Button.H>
+
 #include "MyGLCanvas.h"
 
 using namespace std;
@@ -34,6 +36,7 @@ MyAppWindow *win;
 class MyAppWindow : public Fl_Window {
 public:
 	Fl_Button* wireButton;
+	Fl_Button* drawButton;
 
 	MyGLCanvas* canvas;
 
@@ -60,6 +63,31 @@ private:
 	}
 };
 
+//TODO: SET DRAW SHAPES
+static void radioButtonCB(Fl_Widget* w, void* userdata) {
+		// const char* value = ((Fl_Button*)w)->label();
+		// if (strcmp("Cube", value) == 0) {
+		// 	win->canvas->setShape(SHAPE_CUBE);
+		// }
+		// else if (strcmp("Cylinder", value) == 0) {
+		// 	win->canvas->setShape(SHAPE_CYLINDER);
+		// }
+		// else if (strcmp("Cone", value) == 0) {
+		// 	win->canvas->setShape(SHAPE_CONE);
+		// }
+		// else if (strcmp("Sphere", value) == 0) {
+		// 	win->canvas->setShape(SHAPE_SPHERE);
+		// }
+		// else if (strcmp("Icecream", value) == 0) {
+		// 	win->canvas->setShape(SHAPE_ICECREAM);
+		// }
+		// else if (strcmp("Scene", value) == 0) {
+		// 	win->canvas->setShape(SHAPE_SCENE);
+		// }
+	}
+
+    static void drawShape(){
+    }
 
 MyAppWindow::MyAppWindow(int W, int H, const char*L) : Fl_Window(W, H, L) {
 	begin();
@@ -77,6 +105,39 @@ MyAppWindow::MyAppWindow(int W, int H, const char*L) : Fl_Window(W, H, L) {
 	wireButton = new Fl_Check_Button(0, 0, pack->w() - 20, 20, "Wireframe");
 	wireButton->callback(toggleCB, (void*)(&(canvas->wireframe)));
 	wireButton->value(canvas->wireframe);
+
+
+
+	Fl_Pack* radioPack = new Fl_Pack(w() - 100, 30, 100, h(), "Shape");
+	radioPack->box(FL_DOWN_FRAME);
+	radioPack->labelfont(1);
+	radioPack->type(Fl_Pack::VERTICAL);
+	radioPack->spacing(0);
+	radioPack->begin();
+	{ Fl_Round_Button* tmpButton = new Fl_Round_Button(0, 0, pack->w() - 20, 20, "Cube");
+	tmpButton->type(102);
+	tmpButton->down_box(FL_ROUND_DOWN_BOX);
+	tmpButton->value(1);
+	tmpButton->callback((Fl_Callback*)radioButtonCB);
+	}
+	{ Fl_Round_Button* tmpButton = new Fl_Round_Button(0, 0, pack->w() - 20, 20, "Cylinder");
+	tmpButton->type(102);
+	tmpButton->down_box(FL_ROUND_DOWN_BOX);
+	tmpButton->callback((Fl_Callback*)radioButtonCB);
+	}
+	{ Fl_Round_Button* tmpButton = new Fl_Round_Button(0, 0, pack->w() - 20, 20, "Cone");
+	tmpButton->type(102);
+	tmpButton->down_box(FL_ROUND_DOWN_BOX);
+	tmpButton->callback((Fl_Callback*)radioButtonCB);
+	}
+	{ Fl_Round_Button* tmpButton = new Fl_Round_Button(0, 0, pack->w() - 20, 20, "Sphere");
+	tmpButton->type(102);
+	tmpButton->down_box(FL_ROUND_DOWN_BOX);
+	tmpButton->callback((Fl_Callback*)radioButtonCB);
+	}
+	drawButton = new Fl_Button(0, 0, pack->w() - 20, 20, "Draw Shape");
+	drawButton->callback((Fl_Callback*)drawShape);
+
 
 	end();
 }
